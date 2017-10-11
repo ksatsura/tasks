33e44ade4fc8c1ef24c969bc1533b722c32d9f47
ksatsura <31850962+ksatsura@users.noreply.github.com>
2017-10-11 16:06:44 +0300

    Update compareTrees.js

--- compareTrees.js
+++ compareTrees.js
@@ -2,11 +2,11 @@
 	if (par1 && par2) {              // если узел существует
 	    compareTrees(par1.left, par2.left);       // синхронный обход левых поддеревьев
 		if (par1.data !== par2.data) {
-			    return false;
+		    return false;
 		}
 		compareTrees(par1.right, par2.right);     // синхронный обход правых поддеревьев
 	} else if (!par1 && !par2) {     // если узлы обоих деревьев были пройдены одновременно
-	    return true;
+	      return true;
 	  }
 	return false;       // одно из деревьев имеет больше узлов чем второе
  }
