07f9bab02069d418dd389178320c4ac834b8d369
ksatsura <31850962+ksatsura@users.noreply.github.com>
2017-10-11 16:13:28 +0300

    Update searchString.js

--- searchString.js
+++ searchString.js
@@ -1,11 +1,11 @@
 function searchString(array) {
     var hash = {};          // ассоциативный массив
-	for (var i = 0; i < array.length; i++) {
+    for (var i = 0; i < array.length; i++) {
 	    if (!hash[array[i]]) {       //если ключа нет в ассоциативном массиве
 		 hash[array[i]] = true;
 	    } else if(hash[array[i]]) {   //если ключ уже существует
 		  return array[i];
 	      } 
-	}
-	return null;
+    }
+    return null;
 }
